c7d5f0a3432617eea7befcc2dc716326065e8767
THAIS ARAUJO DOS SANTOS <thais.asantos83@senacsp.edu.br>
2025-07-10 22:17:14 -0300

    inserindo npm

--- 06_http/index.js
+++ 06_http/index.js
@@ -1,3 +1,12 @@
-const http = require('http')
+const http = require('http') //módulo que cria um servidor
 
 const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.write('oi HTTP Thais!')
+    res.end()
+})
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
