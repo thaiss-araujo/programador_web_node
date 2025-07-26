487919d6066fe7c0068842f28fa223427c1facd7
THAIS ARAUJO DOS SANTOS <thais.asantos83@senacsp.edu.br>
2025-07-25 22:15:36 -0300

    aula 25/07

--- alfala_books/backend/controllers/livroController.js
+++ alfala_books/backend/controllers/livroController.js
@@ -1,8 +1,54 @@
+import { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletaLivroPorId} from '../services/livroServices.js';
+
 export const getLivros = (req, res) => {
     try{
-        res.send('Lista de livros');
+        const livros = getTodosLivros();
+        res.send(livros);
     } catch (error) {
         res.status(500);
         res.send(error.message);
+    }
+};
+
+export const getLivro = (req, res) => {
+    try{
+        const id = req.params.id
+        const livro = getLivroPorId(id)
+        res.send(livro)
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+}
+
+export const postLivro = async (req, res) => {
+    try{
+        const livroNovo = req.body;
+        await insereLivro(livroNovo);
+        res.status(201).json(livroNovo);
+    } catch (error){
+        res.status(500).send(error.message);
+    }
+}
+
+export const patchLivro = async (req, res) => {
+    try{
+        const id = req.params.id
+        const body = req.body
+        modificaLivro(body, id)
+        res.send('Livro modificado com sucesso!')
+    } catch{
+        res.status(500).send(error.message);
+    }
+}
+
+export const deletaLivro = (req, res) => {
+    try {
+        const id = req.params.id
+        deletaLivroPorId(id)
+        res.send("livro deletado com sucesso")
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
     }
 };
--- alfala_books/backend/routers/livrosRouter.js
+++ alfala_books/backend/routers/livrosRouter.js
@@ -1,27 +1,13 @@
 import express from 'express'
-import { getLivros } from '../controllers/livroController.js';
+import { getLivros, getLivro, postLivro, patchLivro, deletaLivro } from '../controllers/livroController.js';
 const router = express.Router()
 
-// Rota GET para a raiz do recurso "livros"
-router.get('/', getLivros)
- 
-// Rota POST para a raiz do recurso "livros"
-router.post('/', (req, res) => {
-  // Retorna a mensagem "Você fez uma requisição do tipo POST"
-    res.send('Você fez uma requisição do tipo POST');
-});
- 
-// Rota PATCH para a raiz do recurso "livros"
-router.patch('/', (req, res) => {
-  // Retorna a mensagem "Você fez uma requisição do tipo PATCH"
-    res.send('Você fez uma requisição do tipo PATCH');
-});
- 
-// Rota DELETE para a raiz do recurso "livros"
-router.delete('/', (req, res) => {
-  // Retorna a mensagem "Você fez uma requisição do tipo DELETE"
-    res.send('Você fez uma requisição do tipo DELETE');
-});
- 
+router.delete('/:id', deletaLivro);
+router.patch('/:id', patchLivro);
+router.post('/', postLivro);
+router.get('/', getLivros);
+router.get('/:id', getLivro);
+
+
  
 export default router;
